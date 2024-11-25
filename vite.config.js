c4ce7b48737f4bc9e0a6cb4683494761029fc22b
Bryan Ramirez-Gonzalez <bryanram2024@gmail.com>
2024-11-25 07:12:22 -0800

    Built 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/gesm_final/', // Add your repository name here
   server: {
     proxy: {
       '/api': {
@@ -13,4 +14,3 @@ export default defineConfig({
     }
   }
 })
-
