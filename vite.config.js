a108a835406fe3a6aff1cbf9de36db7cea186510
Bryan Ramirez-Gonzalez <bryanram2024@gmail.com>
2024-12-11 05:37:52 -0800

    Fixing gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  base: '/gesm_final/', // Replace 'my-repo-name' with the name of your GitHub repository
+  build: {
+    outDir: 'dist',
+  },
 })
-
